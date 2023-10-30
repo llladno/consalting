d4a8d0bf9a7e406d6c6b31e87a2c17e5104e7195
llladno <man30968@gmail.com>
2023-10-30 17:33:14 +0300

    backup bd 30.10.2023

---
+++ DB/dump 30.10.2023/company_ordercheck.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: company
+-- ------------------------------------------------------
+-- Server version	8.1.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ordercheck`
+--
+
+DROP TABLE IF EXISTS `ordercheck`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ordercheck` (
+  `idordercheck` int NOT NULL AUTO_INCREMENT,
+  `idclient` int NOT NULL,
+  `idorder` int NOT NULL,
+  `dateordercheck` varchar(45) NOT NULL,
+  `dateorderpay` varchar(45) NOT NULL,
+  `sumordercost` int NOT NULL,
+  `statusordercheck` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`idordercheck`),
+  KEY `idclient` (`idclient`),
+  KEY `idorder` (`idorder`),
+  CONSTRAINT `ordercheck_ibfk_1` FOREIGN KEY (`idclient`) REFERENCES `client` (`idclient`),
+  CONSTRAINT `ordercheck_ibfk_2` FOREIGN KEY (`idorder`) REFERENCES `mainorder` (`idorder`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ordercheck`
+--
+
+LOCK TABLES `ordercheck` WRITE;
+/*!40000 ALTER TABLE `ordercheck` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ordercheck` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-30 17:32:17
